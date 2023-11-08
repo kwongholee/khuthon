292f5d1f70986c179e4a92215119c7f0d54cc65c
kwongholee <ghlee9883@gmail.com>
2023-11-09 00:11:34 +0900

    [add] level-change 디자인, 퀴즈 페이지 디자인(미완성)

---
+++ front/src/components/WordList.jsx
@@ -0,0 +1,35 @@
+import style from '../style/quiz.module.css'
+
+export default function WordList() {
+  return(
+    <div>
+      <div className={style.wordlistBackground}>
+        <div className={style.bookTitle}>
+          <h3>글제목</h3>
+        </div>
+        <div className={style.backgroundColor}>
+          <div className={style.leftWordlist}>
+            <ul>
+              <li>a</li>
+              <li>a</li>
+              <li>a</li>
+              <li>a</li>
+              <li>a</li>
+            </ul>
+          </div>
+
+          <div className={style.rightWordlist}>
+            <ul>
+              <li>a</li>
+              <li>a</li>
+              <li>a</li>
+              <li>a</li>
+              <li>a</li>
+            </ul>
+          </div>
+        </div>
+        <p style={{float: 'right'}}>날짜</p>
+      </div>
+    </div>
+  )
+}
--- front/src/routes/Quiz.jsx
+++ front/src/routes/Quiz.jsx
@@ -1,9 +1,21 @@
 import style from '../style/quiz.module.css';
+import WordList from '../components/WordList';
+import Logo from '../components/Logo';
+import ProfileImage from '../components/ProfileImage';
 
 export default function Quiz() {
   return(
     <div>
-      
+      <div>
+        <Logo></Logo>
+        <ProfileImage></ProfileImage>
+      </div>
+
+      <div style={{background: '#'}}>
+        <div>왼쪽 화살표 이동</div>
+        <WordList></WordList>
+        <div>오른쪽 화살표 이동</div>
+      </div>
     </div>
   )
 }
