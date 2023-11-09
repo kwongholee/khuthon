9c0fbc3b4404a1bd5b2683ffbf6624a43efc9f71
youngju6143 <youngju6143@naver.com>
2023-11-09 11:12:09 +0900

    [edit] logo

--- front/src/components/Logo.jsx
+++ front/src/components/Logo.jsx
@@ -1,5 +1,5 @@
 export default function Logo() {
   return(
-    <div style={{backgroundImage: 'url(logo.png)', width: '100px', height: '100px'}}></div>
+    <div><img style={{width:'248px', height:'145px'}} src='/logo.png'></img></div>
   )
 }
--- front/src/routes/Register.jsx
+++ front/src/routes/Register.jsx
@@ -4,11 +4,39 @@ import Logo from '../components/Logo';
 
 export default function Register() {
   return (
-    <div>
-      {/* <Logo></Logo> */}
-      <img className='로고'></img>
-      <img></img>
-      <p> register</p>
+    <div className={style.container}>
+      <Logo></Logo>
+      <div>
+        <img className={style.profile}></img>
+        <div className={style.name_container}>
+          <input className={style.input_name} placeholder='이름을 입력해주세요.'></input>
+          <p> 이름을 입력해야 합니다! </p>
+        </div>
+        <div className='연령'>
+          <p>연령</p>
+          <select>
+            <option>10세 이하</option>
+            <option>10대</option>
+            <option>20대</option>
+            <option>30대</option>
+            <option>40대</option>
+            <option>50대</option>
+            <option>60대</option>
+            <option>70세 이상</option>
+          </select>
+        </div>
+        <div className='한국어? 언어?'>
+          <button className='한국어'></button>
+          <button className='영어'></button>
+        </div>
+        <div className='이전 다음 버튼'>
+          <button className='이전 버튼'></button>
+          <button className='다음 버튼'></button>
+        </div>
+
+        <p> register</p>
+      </div>
+
     </div>
   )
 }
