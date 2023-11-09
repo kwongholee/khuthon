b23f527c5a3e9dcd664a30953d9e31acd6c022a0
kwongholee <ghlee9883@gmail.com>
2023-11-09 11:32:19 +0900

    [add] 페이지 레이아웃 틀

--- front/src/routes/Wordlist.jsx
+++ front/src/routes/Wordlist.jsx
@@ -1,9 +1,24 @@
 import style from '../style/wordlist.module.css';
+import Logo from '../components/Logo';
+import ProfileImage from '../components/ProfileImage';
 
 export default function Wordlist() {
   return(
     <div>
-      
+      <div>
+        <Logo></Logo>
+        <ProfileImage></ProfileImage>
+      </div>
+
+      <div>
+        <div>
+          검색창
+        </div>
+
+        <div>
+          단어장
+        </div>
+      </div>
     </div>
   )
 }
