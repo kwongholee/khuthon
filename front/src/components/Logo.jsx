44de6a69e810f4e77ff778518107bb353e92dd27
youngju6143 <youngju6143@naver.com>
2023-11-09 10:50:10 +0900

    [edit] background css

--- front/src/components/Logo.jsx
+++ front/src/components/Logo.jsx
@@ -1,5 +1,5 @@
 export default function Logo() {
   return(
-    <div style={{backgroundImage: 'url(/logo.png)'}}></div>
+    <div style={{backgroundImage: 'url(logo.png)', width: '100px', height: '100px'}}></div>
   )
 }
--- front/src/routes/Register.jsx
+++ front/src/routes/Register.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import style from '../style/register.module.css';
+import Logo from '../components/Logo';
 
 export default function Register() {
   return (
     <div>
-
+      {/* <Logo></Logo> */}
+      <img className='로고'></img>
+      <img></img>
+      <p> register</p>
     </div>
   )
 }
