e96019a91f9ef432d6330373b71253e2ab483999
youngju6143 <youngju6143@naver.com>
2023-11-10 14:38:14 +0900

    [edit] rightBtn

--- front/src/components/RightBtn.jsx
+++ front/src/components/RightBtn.jsx
@@ -1,12 +1,13 @@
 import {BsFillArrowRightCircleFill} from 'react-icons/bs'
 import {useNavigate} from 'react-router-dom';
+import style from '../style/arrow.module.css'
 
 export default function RightBtn() {
     let navigate = useNavigate();
 
     return(
         <div>
-            <BsFillArrowRightCircleFill style={{cursor: 'pointer'}} onClick={() => {navigate()}} />
+            <BsFillArrowRightCircleFill className={style.right_icon} onClick={() => {navigate()}} />
         </div>
     )
 }
