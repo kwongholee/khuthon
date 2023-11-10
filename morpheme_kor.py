28958392d0152231aea3fa512dfb35e25e918635
minahkim03 <87796634+minahkim03@users.noreply.github.com>
2023-11-10 19:32:41 +0900

    [add] 형태소 분리

--- morpheme_kor.py
+++ morpheme_kor.py
@@ -8,7 +8,7 @@ os.environ["JAVA_HOME"] = "/Library/Java/JavaVirtualMachines/jdk-1.8.jdk"
 
 #input_json = sys.argv[1]
 #input_array = json.loads(input_json)
-input_array = ["쓰였다."]
+input_array = ["줄거리를"]
 content = ' '.join(input_array)
 result = kkma.morphs(content)
 
